e7b30a12044517bd1c1f64a42056e1f5fdd3fac1
Malec99 <44192555+Malec99@users.noreply.github.com>
2018-10-22 19:07:51 +0100

    Items boyzzz

---
+++ items.py
@@ -0,0 +1,71 @@
+item_id = {
+    "id": "id",
+
+    "name": "id card",
+
+    "description":
+    """You new shiny student ID card. Expires 1 June 2017.
+You wonder why they have printed a suicide hotline number on it?..."""
+}
+
+item_roomKeys = {
+    "id": "roomkeys",
+
+    "name": "Room Keys",
+
+    "description":
+    "Better not to lose them..."
+}
+
+item_wallet = {
+    "id": "wallet",
+
+    "name": "wallet",
+
+    "description":
+    """Lucky you! You still have your wallet after last night. You look inside
+    hoping that you still have money for tuition fee but what's left is 20 quid and receipt
+    from the pub"""
+}
+
+item_bike = {
+    "id": "bike",
+
+    "name": "bike",
+
+    "description": """It's all rusty and someone slashed its tires twice but you still
+love that bike"""
+}
+
+item_bikeKey = {
+    "id": "bike key",
+    
+    "name": "bike key",
+
+    "description": "You have to keep your bike safe"
+}
+
+item_evidence = {
+    "id": "evidence",
+    
+    "name": "a video evidence",
+
+    "description": """VHS recording from your favourite pub's CCTV where
+is evidence of your innocence. Who is still using VHS?"""
+}
+
+item_goldBar = {
+    "id": "goldBar",
+    
+    "name": "GOLD BAR",
+
+    "description": """Well it's a GOLD BAR!!! You can get anything you ever wished.
+You can use it as a bribe"""
+}
+item_planeTicket = {
+    "id": "planeTicket",
+    
+    "name": "a plane ticket to Maldives",
+
+    "description": "A plane ticket for two to Maldives where it's always sunny"
+}
